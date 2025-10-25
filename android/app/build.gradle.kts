f83df1017a2080eda84cd0a7c50ac501e2e552f3
177251 <shriyakjain@gmail.com>
2025-10-25 07:36:59 +0530

    Fixed crashing issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,5 @@
-import java.util.Properties
+import java.util.Properties // Make sure this import line is at the very top
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,50 +7,66 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// --- This block reads your key.properties file ---
 val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
+val keystorePropertiesFile = rootProject.file("key.properties") // Assumes key.properties is in the 'android' folder
 if (keystorePropertiesFile.exists()) {
     keystorePropertiesFile.inputStream().use { keystoreProperties.load(it) }
 }
+// --- End block ---
+
 android {
+    // --- Use your original namespace here if it was different ---
     namespace = "com.shriyakjain.debtledger"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        // --- Updated Java Version ---
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        // --- Updated Kotlin JVM Target ---
+        jvmTarget = "17"
     }
 
+    // --- Defines the signing configurations ---
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            // Updated to handle potential null storeFile path safely
+            storeFile = if (keystoreProperties["storeFile"] != null && keystoreProperties["storeFile"].toString().isNotEmpty()) {
+                file(keystoreProperties["storeFile"] as String)
+            } else {
+                null
+            }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+    // --- End signingConfigs block ---
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.shriyakjain.debtledger"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // --- Use your original application ID here if it was different ---
+        applicationId = "com.shriyakjain.debtledger" // Or "com.example.debtledger"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-    create("release") {
-        keyAlias = keystoreProperties["keyAlias"] as String?
-        keyPassword = keystoreProperties["keyPassword"] as String?
-        storeFile = file(keystoreProperties["storeFile"] as String?)
-        storePassword = keystoreProperties["storePassword"] as String?
-    }
-}
     buildTypes {
-    getByName("release") {
-        // You might have other lines here, like isMinifyEnabled = true
-        signingConfig = signingConfigs.getByName("release")
+        getByName("release") {
+            // --- Corrected signing config ---
+            signingConfig = signingConfigs.getByName("release")
+            // You might add other release settings here later, like:
+            // isMinifyEnabled = true
+            // isShrinkResources = true
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
-}
 }
 
 flutter {
